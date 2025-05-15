4d72e8346969bb563dfb4eaab3343e4618859534
Chase <chasew13579@gmail.com>
2025-05-15 10:00:05 -0700

    Camera added with bounds on X and Y

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 private BoxCollider2D boxCollider;
-
 private Vector3 moveDelta;
+private RaycastHit2D hit;
 private void Start()
 {
     boxCollider = GetComponent<BoxCollider2D>();
@@ -26,7 +26,18 @@ private void FixedUpdate()
     else if (moveDelta.x < 0)
         transform.localScale = new Vector3(-1,1,1);
 
-    //Move
-    transform.Translate(moveDelta * Time.deltaTime);
+    //Check move direction by casting box there first if returns null we can move
+    hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0,moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor","Blocking"));
+    if (hit.collider == null)
+    {
+       //Move
+    transform.Translate(0, moveDelta.y * Time.deltaTime, 0); 
+    }
+    hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor","Blocking"));
+    if (hit.collider == null)
+    {
+       //Move
+    transform.Translate(moveDelta.x * Time.deltaTime, 0, 0); 
+    }
 }
 }
