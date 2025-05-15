a0c2e5e76aec8a16ab0db5bdc7f36f3ae07f8c86
Chase <chasew13579@gmail.com>
2025-05-15 14:25:00 -0700

    Chests added

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : Collidable
+public class Chest : Collectable
 {
-    protected override void OnCollide(Collider2D coll)
+    public Sprite emptyChest;
+    public int coinsAmt = 5;
+    protected override void OnCollect()
     {
-        Debug.Log("Grant Coins");
+        if (!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+            Debug.Log("Grant " + coinsAmt + " coins");
+        }
     }
 }
